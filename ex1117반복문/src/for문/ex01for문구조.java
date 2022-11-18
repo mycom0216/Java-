f3b2052570ae9caee5e302cd831013459ed89217
0122343 <mycom0123456@gmail.com>
2022-11-18 15:05:10 +0900

    for문

---
+++ ex1117반복문/src/for문/ex01for문구조.java
@@ -0,0 +1,47 @@
+package for문;
+
+public class ex01for문구조 {
+
+	public static void main(String[] args) {
+		
+		
+		// 1에서 10까지 출력되는 프로그램
+		
+				// for(초기화; 검사조건; 반복 후 작업)
+				for(int i = 1; i < 11; i++) {
+					System.out.print(i+" ");			
+				}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ ex1117반복문/src/for문/ex02홀수찾기.java
@@ -0,0 +1,16 @@
+package for문;
+
+public class ex02홀수찾기 {
+
+	public static void main(String[] args) {
+
+		for (int i = 96; i > 72; i--) {
+			// i가 홀수일때 만 출력
+			if (i % 2 == 1) {
+				System.out.print(i + " ");
+			}
+		}
+
+	}
+
+}
